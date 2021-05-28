eb030500b933791b1a8520f590cc5eda25d65e8d
thisistrivial <g.lee13770334@gmail.com>
2021-05-28 16:41:38 -0400

    Added journal handler with db actions

---
+++ src/journal_handler.py
@@ -0,0 +1,57 @@
+import dir_handler
+
+import sqlite3
+import time
+
+def get_conn():
+  return sqlite3.connect(dir_handler.get_journal())
+
+def exec_command(comm):
+  conn = get_conn()
+  cursor = conn.cursor()
+  comm(cursor)
+  conn.commit()
+  conn.close()
+
+def create_entries_table(cursor):
+  comm = '''CREATE TABLE IF NOT EXISTS entries (
+              id integer PRIMARY KEY,
+              creation_time integer NOT NULL,
+              last_edit_time integer NOT NULL,
+              entry text
+            )'''
+  cursor.execute(comm)
+
+def insert_entry(cursor, entry):
+  comm = '''INSERT INTO entries (
+              creation_time,
+              last_edit_time,
+              entry
+            )
+            VALUES (?, ?, ?)'''
+  now = int(time.time())
+  cursor.execute(comm, (now, now, entry))
+
+def update_entry(cursor, pk, entry):
+  comm = '''UPDATE entries SET
+              last_edit_time = ?,
+              entry = ?
+            WHERE id = ?'''
+  now = int(time.time())
+  cursor.execute(comm, (now, entry, pk))
+
+def delete_entry(cursor, pk):
+  comm = 'DELETE FROM entries WHERE id = ?'
+  cursor.execute(comm, (pk,))
+
+def get_all_entries(cursor):
+  comm = 'SELECT * FROM entries'
+  cursor.execute(comm)
+  rows = cursor.fetchall()
+  return rows
+
+def get_entry(cursor, pk):
+  comm = 'SELECT * FROM entries WHERE id = ?'
+  cursor.execute(comm, (pk,))
+  rows = cursor.fetchall()
+  return rows
